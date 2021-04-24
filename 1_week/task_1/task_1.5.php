9945bf6960e3d13574836c0b91a35bf6db57b9b3
Galushka Vladislav <galushka.vp@gmail.com>
2021-04-24 14:08:22 +0700

    create task 1.5

---
+++ 1_week/task_1/task_1.5.php
@@ -0,0 +1,29 @@
+<?php
+
+$bmw = [
+    'model' => 'X5',
+    'spped' => 120,
+    'doors' => 5,
+    'year' => 2015
+];
+$toyota = [
+    'model' => 'RAV4',
+    'spped' => 160,
+    'doors' => 5,
+    'year' => 2019
+];
+$opel = [
+    'model' => 'Mokka',
+    'spped' => 120,
+    'doors' => 5,
+    'year' => 2014
+];
+
+$cars['bmw'] = $bmw;
+$cars['toyota'] = $toyota;
+$cars['opel'] = $opel;
+
+foreach ($cars as $carName => $carData) {
+    echo "CAR $carName<br>\n";
+    echo "{$carData['model']} {$carData['spped']} {$carData['doors']} {$carData['year']}<br><br>\n\n";
+}
